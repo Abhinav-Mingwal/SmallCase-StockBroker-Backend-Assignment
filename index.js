7960153f2c034f1dc9a2fd6a0373cbe1f9714997
Abhinav-Mingwal <abhinav.mingwal@gmail.com>
2022-03-07 18:32:53 +0530

    addded heroku mongo atlas

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 app.use('/api', require('./routes/api'))
 
-var mongoDB = "mongodb+srv://abhinav:WxXWkxDsCDGsjkQq@cluster0.se7xo.mongodb.net/stockBroker?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URL
 
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true},()=>{
     console.log("Connected to Database")
